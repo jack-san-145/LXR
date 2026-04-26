dd9b9b764b3036ef3b3c0dcf9de02e2f797a9c7e
jack-san-145 <santhosh34848550@gmail.com>
2026-04-26 15:40:14 +0000

    Add routes in seperate chi router with proper grouping

---
+++ cmd/server/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"lxr-d/internal/handlers"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func NewRouter(lxr *handlers.LXRHandler) *chi.Mux {
+
+	r := chi.NewRouter()
+
+	r.Get("/ping", lxr.PingHanlder)
+
+	r.Post("/create", lxr.CreateHandler)
+	return r
+}
