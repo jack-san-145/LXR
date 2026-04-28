9f95e18a82782bf05ba4dea9bf0bace747745b63
jack-san-145 <santhosh34848550@gmail.com>
2026-04-28 14:41:21 +0000

    Add comments on container-setup

--- script/container-setup.sh
+++ script/container-setup.sh
@@ -2,33 +2,40 @@
 
 set -ex
 
+#directory to store the container's data
 ROOT_FS=/home/jack/LXR-data/$CONTAINER_NAME-$CONTAINER_ID/rootfs
 
+#after this mount wont affects the host 
 mount --make-rprivate /
 
+#make the same directory as mount point
 mount --bind $ROOT_FS $ROOT_FS
  
+#create /dev inside the new rootfs
 mkdir -p $ROOT_FS/dev
 
+#create null inside new rootfs
 touch $ROOT_FS/dev/null
 
+#mount the host's /dev/null to new rootfs /dev/null
 mount --bind /dev/null $ROOT_FS/dev/null
 
-#ls -l $ROOT_FS
-
+#create directory named old_root to store the old root(host rootfs)
 mkdir -p $ROOT_FS/old_root
 
+#it changes the new rootfs as (/ root directory) and move host rootfs to old_root
 pivot_root $ROOT_FS $ROOT_FS/old_root
 
 cd /
 
+#mount proc (virtual filesystem)
 mount -t proc proc /proc
 
+#clear old rootfs inside container
 umount -l /old_root
 
+#mount temporary fs to /tmp
 mount -t tmpfs tmpfs /tmp
 
-#hostname $HOSTNAME
-
-# run container app
+#root process of the container
 exec /bin/bash
