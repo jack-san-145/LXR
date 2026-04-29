96ce86172036953b2c6eb9c3d5ca7f3ea37f38ff
jack-san-145 <santhosh34848550@gmail.com>
2026-04-29 13:57:22 +0000

    Listen the interrupt signal from main function and Run server with seperate go routine

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"log"
 	"lxr-d/internal/handlers"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
 	"os/user"
 	"strconv"
+	"syscall"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -41,9 +46,20 @@ func main() {
 
 	LXRHandler := handlers.NewHandler()
 	router := NewRouter(LXRHandler)
+	go runServer(router, listener)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+
+	<-ctx.Done()
+	LXRHandler.Helper.SaveContainerState()
+	defer stop()
+
+}
+
+func runServer(router *chi.Mux, listener net.Listener) {
 	//start the server
 	log.Println("Server Listening ....")
-	err = http.Serve(listener, router)
+	err := http.Serve(listener, router)
 	if err != nil {
 		log.Fatal("Server Failed to start: ", err)
 	}
