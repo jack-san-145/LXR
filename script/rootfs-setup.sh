d6fd8a4e3e12be322635222e8c6b35e2d2b95f0a
jack-san-145 <santhosh34848550@gmail.com>
2026-04-30 15:57:37 +0000

    Add comments on each script in container setup

--- script/rootfs-setup.sh
+++ script/rootfs-setup.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
+#make the output tracable
 set -ex
 
+#create seperate directory for each container with its containerName+containerID
 mkdir -p /home/jack/LXR-data/$CONTAINER_NAME-$CONTAINER_ID
 
+#enter into it
 cd /home/jack/LXR-data/$CONTAINER_NAME-$CONTAINER_ID
 
+#make rootfs directory
 mkdir rootfs
 
+#Create temporary container
 docker create --name $CONTAINER_NAME $IMAGE_NAME
 
+#extract container's rootfs
 docker export $CONTAINER_NAME > $CONTAINER_NAME-roofs.tar
 
+#extract the tar to get actual rootfs
 tar -xf $CONTAINER_NAME-roofs.tar -C rootfs
 
+#change the rootfs ownership
 chown jack:lxr -R rootfs
 
+#give permissions for /dev to create null further
 chmod 775 rootfs/dev
 
-#ls -l rootfs
-
+#remove created temporary container
 docker rm $CONTAINER_NAME
 
+#remove rootfs tar
 rm -rf $CONTAINER_NAME-roofs.tar
