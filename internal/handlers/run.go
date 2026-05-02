9e26ac2a54a2a583bcca18fa05167bdccb2472b4
jack-san-145 <santhosh34848550@gmail.com>
2026-05-02 11:34:35 +0000

    Access container setup  through helper in handlers

--- internal/handlers/run.go
+++ internal/handlers/run.go
@@ -1,13 +1,8 @@
 package handlers
 
 import (
-	"fmt"
-	"lxr-d/internal/models"
 	"lxr-d/internal/response"
 	"net/http"
-	"os"
-	"os/exec"
-	"syscall"
 )
 
 func (h *Handler) RunHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +10,7 @@ func (h *Handler) RunHandler(w http.ResponseWriter, r *http.Request) {
 	con_name := r.URL.Query().Get("container_name")
 
 	if container, ok := h.Helper.ContainerManager.AllContainers[con_name]; ok {
-		err := ContainerSetup(container)
+		err := h.Helper.ContainerSetup(container)
 		if err != nil {
 			response.WriteJson(w, map[string]bool{"active": false})
 			return
@@ -23,45 +18,4 @@ func (h *Handler) RunHandler(w http.ResponseWriter, r *http.Request) {
 		h.Helper.ContainerManager.ActiveContainers[container.ContainerName] = &container.PID
 		response.WriteJson(w, map[string]bool{"active": true})
 	}
-}
-
-func ContainerSetup(container *models.Container) error {
-
-	container_name_env := "CONTAINER_NAME=" + container.ContainerName
-	container_id_env := "CONTAINER_ID=" + container.ContainerId
-
-	cmd := exec.Command(
-		"unshare",
-		"--pid",
-		"--mount",
-		"--map-root-user",
-		"--fork",
-		"--",
-		"bash",
-		"../../script/container-setup.sh",
-	)
-
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Credential: &syscall.Credential{
-			Uid: 1000, // non-root
-			Gid: 1000,
-		},
-	}
-
-	cmd.Env = append(os.Environ(),
-		container_name_env,
-		container_id_env)
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err := cmd.Start()
-	if err != nil {
-		fmt.Println("Error container setup : ", err)
-		return err
-	}
-	container.PID = cmd.Process.Pid
-	fmt.Println("container Pid = ", container.PID)
-
-	return nil
 }
