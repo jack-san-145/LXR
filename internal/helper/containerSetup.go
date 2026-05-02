6e846ba1710b98a6869b23cf41022cef4d0bf979
jack-san-145 <santhosh34848550@gmail.com>
2026-05-02 11:33:59 +0000

    Seperate container setup to helper

---
+++ internal/helper/containerSetup.go
@@ -0,0 +1,53 @@
+package helper
+
+import (
+	"fmt"
+	"lxr-d/internal/models"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func (h *Helper) ContainerSetup(container *models.Container) error {
+
+	container_name_env := "CONTAINER_NAME=" + container.ContainerName
+	container_id_env := "CONTAINER_ID=" + container.ContainerId
+
+	cmd := exec.Command(
+		"unshare",
+		"--pid",
+		"--mount",
+		"--map-root-user",
+		"--fork",
+		"--",
+		"bash",
+		"../../script/container-setup.sh",
+	)
+
+	//switch non-root to spawn containers
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Credential: &syscall.Credential{
+			Uid: 1000, // non-root
+			Gid: 1000,
+		},
+	}
+
+	//inject env to the script
+	cmd.Env = append(os.Environ(),
+		container_name_env,
+		container_id_env)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	//run script in background
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println("Error container setup : ", err)
+		return err
+	}
+	container.PID = cmd.Process.Pid
+	fmt.Println("container Pid = ", container.PID)
+
+	return nil
+}
