50cc7f97948cd012faf8dd81cfbdafe06a6eaea2
jack-san-145 <santhosh34848550@gmail.com>
2026-05-03 02:38:38 +0000

    copy image rootfs to container data and remove docker create ,export to extract rootfs

--- script/rootfs-setup.sh
+++ script/rootfs-setup.sh
@@ -4,31 +4,16 @@
 set -ex
 
 #create seperate directory for each container with its containerName+containerID
-mkdir -p /home/jack/LXR-data/$CONTAINER_NAME-$CONTAINER_ID
+mkdir -p /home/LXR/LXR-data/$CONTAINER_NAME-$CONTAINER_ID
+
+#copy image rootfs Recursively to container data
+cp -r /home/LXR/LXR-registry/$IMAGE_NAME /home/LXR/LXR-data/$CONTAINER_NAME-$CONTAINER_ID/
 
 #enter into it
-cd /home/jack/LXR-data/$CONTAINER_NAME-$CONTAINER_ID
-
-#make rootfs directory
-mkdir rootfs
-
-#Create temporary container
-docker create --name $CONTAINER_NAME $IMAGE_NAME
-
-#extract container's rootfs
-docker export $CONTAINER_NAME > $CONTAINER_NAME-roofs.tar
-
-#extract the tar to get actual rootfs
-tar -xf $CONTAINER_NAME-roofs.tar -C rootfs
+cd /home/LXR/LXR-data/$CONTAINER_NAME-$CONTAINER_ID
 
 #change the rootfs ownership
 chown jack:lxr -R rootfs
 
 #give permissions for /dev to create null further
 chmod 775 rootfs/dev
-
-#remove created temporary container
-docker rm $CONTAINER_NAME
-
-#remove rootfs tar
-rm -rf $CONTAINER_NAME-roofs.tar
