15e9e70897429315136c25160adc71780c5c921c
jack-san-145 <santhosh34848550@gmail.com>
2026-05-05 20:52:39 +0000

    Add Start response in models

--- internal/models/response.go
+++ internal/models/response.go
@@ -4,4 +4,12 @@ type CreationResponse struct {
 	ContainerId   string `json:"container_id"`
 	ContainerName string `json:"container_name"`
 	IsCreated     bool   `json:"is_created"`
+	AlreadyExists bool   `json:"already_exists"`
+}
+
+type StartResponse struct {
+	AlreadyActive bool `json:"already_active"`
+	Activated     bool `json:"activated"`
+	Failed        bool `json:"failed"`
+	DoesNotExists bool `json:"exists"`
 }
