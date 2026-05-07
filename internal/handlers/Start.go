254c42ada111de7ddb4f9ee744ec8da95b1f80e6
jack-san-145 <santhosh34848550@gmail.com>
2026-05-07 14:50:21 +0000

    Update seperate struct field instead of raw status msg

--- internal/handlers/Start.go
+++ internal/handlers/Start.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"lxr-d/internal/models"
 	"lxr-d/internal/response"
 	"net/http"
 )
@@ -9,13 +10,31 @@ func (h *Handler) StartHandler(w http.ResponseWriter, r *http.Request) {
 
 	con_name := r.URL.Query().Get("container_name")
 
+	active := h.Helper.ContainerActive(con_name)
+	if active {
+		response.WriteJson(w, models.StartResponse{
+			AlreadyActive: true,
+		})
+		return
+	}
 	if container, ok := h.Helper.ContainerManager.AllContainers[con_name]; ok {
+
 		err := h.Helper.ContainerSetup(container)
 		if err != nil {
-			response.WriteJson(w, map[string]bool{"active": false})
+			response.WriteJson(w, models.StartResponse{
+				Failed: true,
+			})
 			return
 		}
 		h.Helper.ContainerManager.ActiveContainers[container.ContainerName] = &container.PID
-		response.WriteJson(w, map[string]bool{"active": true})
+
+		response.WriteJson(w, models.StartResponse{
+			Activated: true,
+		})
+		return
+
 	}
+	response.WriteJson(w, models.StartResponse{
+		DoesNotExists: true,
+	})
 }
