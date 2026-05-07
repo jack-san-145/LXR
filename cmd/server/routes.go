9c161d6e6274794ecca9117a2f92dc7aefc1e1cd
jack-san-145 <santhosh34848550@gmail.com>
2026-05-07 16:26:27 +0000

    Add stop route in router

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -14,6 +14,7 @@ func NewRouter(h *handlers.Handler) *chi.Mux {
 
 	r.Post("/create", h.CreateHandler)
 	r.Get("/start", h.StartHandler)
+	r.Get("/stop", h.StopHandler)
 	r.Get("/exec", h.ExecHandler)
 	r.Post("/pull_image", h.PullImageHandler)
 	return r
