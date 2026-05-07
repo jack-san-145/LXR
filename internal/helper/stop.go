a6747198993b98b23e10d91071a8282fb30cd145
jack-san-145 <santhosh34848550@gmail.com>
2026-05-07 16:24:49 +0000

    Add Stop process to kill the container pid

---
+++ internal/helper/stop.go
@@ -0,0 +1,25 @@
+package helper
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+func (h *Helper) StopProcess(name string) (bool, error) {
+
+	pid, exists := h.GetContainerPid(name)
+	if !exists {
+		log.Println("Process does not exists")
+		return exists, nil
+	}
+	pid_int, _ := strconv.Atoi(pid)
+	ps, err := os.FindProcess(pid_int)
+
+	if err != nil {
+		return exists, err
+	}
+
+	err = ps.Kill()
+	return exists, err
+}
