bcbf3f9f81fd0b82d7210e5929b438e6d50bbba6
jack-san-145 <santhosh34848550@gmail.com>
2026-05-10 01:26:02 +0000

    Use isempty to check the stack is empty or not

--- internal/ip/stack.go
+++ internal/ip/stack.go
@@ -19,10 +19,17 @@ type Stack struct {
 	Stack []string
 }
 
-func (s Stack) Push(ip string) {
-	s.Stack = append(s.Stack, ip)
-}
-
 func (s Stack) Size() int {
 	return len(s.Stack)
+}
+
+func (s Stack) IsEmpty() bool {
+	if s.Size() != 0 {
+		return false
+	}
+	return true
+}
+
+func (s Stack) Push(ip string) {
+	s.Stack = append(s.Stack, ip)
 }
