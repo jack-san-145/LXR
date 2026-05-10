419db13fe40cefe5fc02d8eb1dc5fa6659539a2c
jack-san-145 <santhosh34848550@gmail.com>
2026-05-10 02:34:00 +0000

    Integrate Ipstack in helper

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -1,13 +1,18 @@
 package helper
 
-import "lxr-d/internal/models"
+import (
+	"lxr-d/internal/ip"
+	"lxr-d/internal/models"
+)
 
 type Helper struct {
 	ContainerManager *models.ContainerManager
+	IpStack          *ip.IpStack
 }
 
-func NewHelper() *Helper {
+func NewHelper(stack *ip.IpStack) *Helper {
 	return &Helper{
 		ContainerManager: models.NewContainerManager(),
+		IpStack:          ip.NewIpStack(*stack),
 	}
 }
