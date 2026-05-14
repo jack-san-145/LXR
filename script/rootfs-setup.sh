0f34d0dac712c0c424ed6e6bebeba625714c9418
jack-san-145 <santhosh34848550@gmail.com>
2026-05-14 14:49:16 +0000

    Declare ip_libraries arr to store all the ip dependency libraries with dynamic loader

--- script/rootfs-setup.sh
+++ script/rootfs-setup.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 #make the output tracable
@@ -17,10 +18,27 @@ cp /usr/bin/ping /home/LXR/LXR-data/$CONTAINER_NAME-$CONTAINER_ID/$IMAGE_NAME/ro
 
 cp /usr/bin/ip /home/LXR/LXR-data/$CONTAINER_NAME-$CONTAINER_ID/$IMAGE_NAME/rootfs/usr/bin
 
-#copy missed libraries from host to container to use ip cmd
-cp /lib/aarch64-linux-gnu/libbpf.so.0 rootfs/lib/aarch64-linux-gnu/
-cp /lib/aarch64-linux-gnu/libelf.so.1 rootfs/lib/aarch64-linux-gnu/
-cp /lib/aarch64-linux-gnu/libmnl.so.0 rootfs/lib/aarch64-linux-gnu/
+
+#to copy missing libraries from host to container to use ip cmd
+#create an indexed array to store ip libraries
+declare -a ip_libraries
+
+container_rootfs=/home/LXR/LXR-data/$CONTAINER_NAME-$CONTAINER_ID/$IMAGE_NAME/rootfs
+
+#append all libraries required for ip iperation to arr
+$ip_libraries+=("/lib/aarch64-linux-gnu/libbpf.so.0")
+$ip_libraries+=("/lib/aarch64-linux-gnu/libelf.so.1")
+$ip_libraries+=("/lib/aarch64-linux-gnu/libmnl.so.0")
+$ip_libraries+=("/lib/aarch64-linux-gnu/libbsd.so.0")
+$ip_libraries+=("/lib/aarch64-linux-gnu/libcap.so.2")
+$ip_libraries+=("/lib/aarch64-linux-gnu/libc.so.6")
+$ip_libraries+=("/lib/aarch64-linux-gnu/libz.so.1")
+$ip_libraries+=("/lib/aarch64-linux-gnu/libmd.so.0")
+
+#loader that loads all the libraries dynamically
+dynamic_loader=/lib/ld-linux-aarch64.so.1 
+
+
 
 #change the rootfs ownership
 chown jack:lxr -R rootfs
