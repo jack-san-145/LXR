39bf3e1b937f995e36eb970bbf1abd73fa195bee
jack-san-145 <santhosh34848550@gmail.com>
2026-05-15 06:38:53 +0000

    Allocate ip for container and bring container and lo interface up

--- script/ip-setup.sh
+++ script/ip-setup.sh
@@ -14,6 +14,17 @@ ip link set $BRIDGE_VETH up
 #move another side veth to container from host
 ip link set $CONTAINER_VETH netns $CONTAINER_PID
 
+#allocate ip address for container
+nsenter -t $CONTAINER_PID --net \
+    ip addr add $CONTAINER_IP dev $CONTAINER_VETH
+
+#bring container interface up
+nsenter -t $CONTAINER_PID --net \
+    ip link set $CONTAINER_VETH up
+
+#bring conainer's loopback interface up
+nsenter -t $CONTAINER_PID --net \
+    ip link set lo up
 
 
 
