01882159692bf4b862b1a1b9cf43517ddbcdca6f
jack-san-145 <santhosh34848550@gmail.com>
2026-05-16 16:04:08 +0000

    Add method FindLargestIP to find an largest ip among two

--- internal/ip/ipHelper.go
+++ internal/ip/ipHelper.go
@@ -1,7 +1,11 @@
 package ip
 
-import ("fmt"
-"log")
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
 
 // return lxr's default bridge 'lxr0'
 func (nc *NetworkConfig) UseBridge() string {
@@ -22,3 +26,27 @@ func (nc *NetworkConfig) MakeIp(first, second, third, fourth int) string {
 	return fmt.Sprintf("%v.%v.%v.%v/%v", first, second, third, fourth, nc.CIDR)
 }
 
+func (nc *NetworkConfig) FindLargestIP(IP1, IP2 string) string {
+
+	//split both ip's as octet array
+	IP1_arr := strings.Split(IP1, ".")
+	IP2_arr := strings.Split(IP2, ".")
+
+	//traverse ip array
+	for i := 0; i < len(IP1_arr); i++ {
+
+		//convert string octet to int
+		ip1_octet, _ := strconv.Atoi(IP1_arr[i])
+		ip2_octet, _ := strconv.Atoi(IP2_arr[i])
+
+		//return largest ip as ip1 if ip1 octet is greater
+		if ip1_octet > ip2_octet {
+			return IP1
+		} else if ip2_octet > ip1_octet { //return largest ip as ip2 if ip2 octet is greater
+			return IP2
+		}
+		//if both actet are same do nothing, simply continue iteration
+	}
+	//if for IPs have same octets,then return any one ip from it(both are same)
+	return IP1
+}
