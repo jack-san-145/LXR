5e873c3aa6307e34bd1189b917d9d92b950aa787
jack-san-145 <santhosh34848550@gmail.com>
2026-05-19 19:34:35 +0000

    Install code server to use container via vscode web

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -23,3 +23,9 @@ apt install iproute2 -y
 
 #install ping globally orelse between containers
 apt install iputils-ping 
+
+#install code server to use vscode on web
+curl -fsSL https://code-server.dev/install.sh | sh 
+
+#start code-server in background 
+code-server --bind-addr 0.0.0.0:8080 &
