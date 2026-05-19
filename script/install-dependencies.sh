92e5d7156009b7ad05d49da2a9edd42452583624
jack-san-145 <santhosh34848550@gmail.com>
2026-05-19 19:36:04 +0000

    Start and kill code-server to manipulate password

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -29,3 +29,23 @@ curl -fsSL https://code-server.dev/install.sh | sh
 
 #start code-server in background 
 code-server --bind-addr 0.0.0.0:8080 &
+
+#immediately store code-server's background pid to kill
+cs_pid=$!
+
+#wait for 2 seconds to ensure server run
+sleep 2
+
+#kill code-server pid 
+kill -2 $cs_pid
+
+#when code-server starts that created directory with config.yaml 
+#so after that code-server pid killed and now change code-server's password to config file
+
+>  ~/.config/code-server/config.yaml cat << EOF
+bind-addr: 127.0.0.1:8080
+auth: password
+password: $PASSWORD
+cert: false
+EOF
+
