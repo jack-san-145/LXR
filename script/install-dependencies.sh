60f8cef96b5537dcbcd81ea318c1eb0e0c375885
jack-san-145 <santhosh34848550@gmail.com>
2026-05-19 19:33:40 +0000

    install dependencies (nano,ip,ping) for container

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -11,3 +11,15 @@ chmod 1777 /tmp
 #create both randam and urandom for random number generation
 mknod -m 666 /dev/random c 1 8
 mknod -m 666 /dev/urandom c 1 9
+
+#update apt 
+apt update 
+
+#install nano editor
+apt install nano
+
+#install ip package to use all the iptables oerations
+apt install iproute2 -y 
+
+#install ping globally orelse between containers
+apt install iputils-ping 
