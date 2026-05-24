e1f000f0b844be2a81839f0af513a631ec381f66
jack-san-145 <santhosh34848550@gmail.com>
2026-05-24 07:21:24 +0000

    feat(api): hijack connection for seamless container streaming instead of req-res

--- internal/handlers/create.go
+++ internal/handlers/create.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 	"log"
 	"lxr-d/internal/models"
-	"lxr-d/internal/response"
 	"net/http"
 )
 
 func (h *Handler) CreateHandler(w http.ResponseWriter, r *http.Request) {
 
+	log.Println("comming inside create")
 	var con models.Container
 	err := json.NewDecoder(r.Body).Decode(&con)
 	if err != nil {
@@ -17,32 +17,25 @@ func (h *Handler) CreateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//check the container already exists or not
-	exists := h.Helper.ContainerExists(con.ContainerName)
-	if exists {
-		response.WriteJson(w, models.CreationResponse{
-			IsCreated:     false,
-			AlreadyExists: true,
-		})
+	//hijack the http connnection and stream i/o in real-time over uds
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Hijack not supported", http.StatusInternalServerError)
 		return
 	}
-
-	//if not already exists creats new container environment (only setup containers rootfs)
-	err = h.Helper.RootfsSetup(&con)
+	conn, buf, err := hijacker.Hijack()
 	if err != nil {
-		log.Println("Error during RootfsSetup: ", err)
-		response.WriteJson(w, models.CreationResponse{IsCreated: false})
+		log.Println("Hijack error:", err)
 		return
 	}
+	defer conn.Close()
 
-	//send container creattion response to client
-	response.WriteJson(w, models.CreationResponse{
-		IsCreated:     true,
-		ContainerName: con.ContainerName,
-		ContainerId:   con.ContainerId,
-	})
+	//write http success response manually
+	buf.WriteString("HTTP/1.1 200 OK\r\n")
+	buf.WriteString("Content-Type: text/plain\r\n")
+	buf.WriteString("\r\n")
+	buf.Flush()
 
-	//add newly created container to allContainers
-	h.Helper.ContainerManager.AllContainers[con.ContainerName] = &con
+	conn.Write([]byte("creation started...\n"))
 
 }
