40f6791ef71eb68dd8a3c0383852c2fe1915be50
jack-san-145 <santhosh34848550@gmail.com>
2026-05-24 09:34:05 +0000

    refactor : stream image pull logs to client in real-time

--- internal/helper/imagePull.go
+++ internal/helper/imagePull.go
@@ -1,30 +1,51 @@
 package helper
 
 import (
+	"io"
 	"log"
+	"lxr-d/internal/models"
 	"os"
 	"os/exec"
 )
 
-func (h *Helper) PullImage(image string) (bool, error) {
+func (h *Helper) PullImage(conCreater *models.ContainerCreater) error {
 
-	exits := h.CheckImageLocally(image)
-	if exits {
-		return true, nil
-	}
-	image_env := "IMAGE=" + image
+	image_env := "IMAGE=" + conCreater.Container.Image
 	cmd := exec.Command("../../script/pull-image.sh")
 	cmd.Env = append(cmd.Environ(),
 		image_env,
 	)
 
+	//redirects stdout out and err to terminal
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
+	// Get a pipe to read the command's standard output and error (stdout and stderr)
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+
+	// Stream stdoud(normal output) from the command to the client
+	go func() {
+		io.Copy(conCreater.Conn, stdout)
+	}()
+
+	// Stream stderr(error/warning output) from the command to the client
+	go func() {
+		io.Copy(conCreater.Conn, stderr)
+	}()
+
+	//start to run script at background
+	err := cmd.Start()
 	if err != nil {
-		log.Println("Error rootfs setup : ", err)
-		return false, err
+		log.Println("Image script Error : ", err)
+		return err
 	}
-	return false, nil
+
+	//wait until script to complete
+	err = cmd.Wait()
+	if err != nil {
+		log.Println("Image pull Error: ", err)
+		return err
+	}
+	return nil
 }
