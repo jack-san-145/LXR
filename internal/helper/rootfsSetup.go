6b1374a0465d1725f6e67b2e44924bd7285da878
jack-san-145 <santhosh34848550@gmail.com>
2026-05-24 09:53:21 +0000

    feat(rootfs): use isolated goroutines to stream setup logs to client

--- internal/helper/rootfsSetup.go
+++ internal/helper/rootfsSetup.go
@@ -1,13 +1,13 @@
 package helper
 
 import (
-	"fmt"
+	"github.com/google/uuid"
+	"io"
+	"log"
 	"lxr-d/internal/models"
 	"os"
 	"os/exec"
 	"strings"
-
-	"github.com/google/uuid"
 )
 
 func (h *Helper) RootfsSetup(conCreater *models.ContainerCreater) error {
@@ -19,15 +19,6 @@ func (h *Helper) RootfsSetup(conCreater *models.ContainerCreater) error {
 	image_name_env := "IMAGE_NAME=" + conCreater.Container.Image
 	container_id_env := "CONTAINER_ID=" + conCreater.Container.ContainerId
 
-	// //to check the image locally
-	// exists := h.CheckImageLocally(conCreater.Container.Image)
-	// if !exists {
-	// 	_, err := h.PullImage(conCreater.Container.Image)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
-
 	//run the script with env
 	cmd := exec.Command("../../script/rootfs-setup.sh")
 	cmd.Env = append(os.Environ(),
@@ -38,9 +29,31 @@ func (h *Helper) RootfsSetup(conCreater *models.ContainerCreater) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
+	// Get a pipe to read the command's standard output and error (stdout and stderr)
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+
+	// Stream stdoud(normal output) from the command to the client
+	go func() {
+		io.Copy(conCreater.Conn, stdout)
+	}()
+
+	// Stream stderr(error/warning output) from the command to the client
+	go func() {
+		io.Copy(conCreater.Conn, stderr)
+	}()
+
+	//start to run script at background
+	err := cmd.Start()
 	if err != nil {
-		fmt.Println("Error rootfs setup : ", err)
+		log.Println("rootfs setup Error: ", err)
+		return err
+	}
+
+	//wait until script to complete
+	err = cmd.Wait()
+	if err != nil {
+		log.Println("rootfs setup Error: ", err)
 		return err
 	}
 
