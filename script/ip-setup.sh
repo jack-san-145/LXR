f05428e96fc2316c433da8289f97cdc7f1781583
jack-san-145 <santhosh34848550@gmail.com>
2026-05-26 09:51:07 +0000

    chore(network): add log statements to trace container networking process

--- script/ip-setup.sh
+++ script/ip-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set -ex
+# set -ex
 
+echo -e "[+]creating new veth pairs ✔"
 #creationg new veth pairs 
 ip link add $CONTAINER_VETH type veth peer $BRIDGE_VETH
 
+echo -e "[+]attach to lxr bridge ✔"
 #attach one side veth to bridge lxr0
 ip link set $BRIDGE_VETH master lxr0
 
@@ -26,6 +28,7 @@ nsenter -t $CONTAINER_PID --net \
 nsenter -t $CONTAINER_PID --net \
     ip link set lo up
 
+echo -e "[+]add container default ghateway ✔"
 #add default route as bridge to route unknown traffic
 nsenter -t $CONTAINER_PID --net \
     ip route add default via $BRIDGE_IP
