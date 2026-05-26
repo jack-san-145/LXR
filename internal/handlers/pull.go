c58c0638362bd1cb7b7bd56d89d1a82c3abd85ea
jack-san-145 <santhosh34848550@gmail.com>
2026-05-26 09:20:48 +0000

    feat(pull): integrate image removal logic into the pull handler

--- internal/handlers/pull.go
+++ internal/handlers/pull.go
@@ -20,12 +20,17 @@ func (h *Handler) PullImageHandler(w http.ResponseWriter, r *http.Request) {
 		buf     *bufio.ReadWriter
 	)
 
+	//initialize builder values
+	builder.Container = &models.Container{}
+	builder.Quit = make(chan struct{})
+
 	err := json.NewDecoder(r.Body).Decode(&image)
 	if err != nil {
 		log.Println("Image Pull Error: ", err)
 		return
 	}
 
+	log.Println("image name : ", image.ImageName)
 	builder.Container.Image = image.ImageName
 	//hijack the http connnection and stream i/o in real-time over uds
 	hijacker, ok := w.(http.Hijacker)
@@ -38,7 +43,6 @@ func (h *Handler) PullImageHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hijack error:", err)
 		return
 	}
-	defer builder.Conn.Close()
 
 	//write http success response manually
 	buf.WriteString("HTTP/1.1 200 OK\r\n")
@@ -65,9 +69,11 @@ func (h *Handler) PullImageHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		builder.Conn.Write([]byte("Error in Image Pull\n"))
 		builder.Quit <- struct{}{}
+
+		h.Helper.RemoveImage(builder.Container.ContainerName) //remove image when error occured
 		return
 	}
-	builder.Conn.Write([]byte("Image Pulled Successfully..\n"))
+	// builder.Conn.Write([]byte("Image Pulled Successfully..\n"))
 	builder.Quit <- struct{}{}
 
 }
