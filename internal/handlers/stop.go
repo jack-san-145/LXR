dc62d4c4b1acda762856edaabcbaab4cf31023ad
jack-san-145 <santhosh34848550@gmail.com>
2026-05-26 17:26:07 +0000

    feat(lifecycle): replace stop container logic with cgroup freeze

--- internal/handlers/stop.go
+++ internal/handlers/stop.go
@@ -8,18 +8,33 @@ import (
 
 func (h *Handler) StopHandler(w http.ResponseWriter, r *http.Request) {
 
-	con_name := r.URL.Query().Get("container_name")
+	containerName := r.URL.Query().Get("container_name")
 
-	exists, err := h.Helper.StopContainer(con_name)
-	if !exists || err != nil {
+	//check whether the container is active or not
+	active := h.Helper.IsContainerActive(containerName)
+	if active {
+		h.Helper.FreezeContainer(containerName)
 		response.WriteJson(w, models.StopResponse{
-			Exists:  exists,
-			Stopped: false,
+			Exists:  true,
+			Stopped: true,
 		})
+		return
 	}
 
+	//check whether the container is exists or not
+	exists := h.Helper.ContainerExists(containerName)
+	if exists {
+		response.WriteJson(w, models.StopResponse{
+			Exists:  true,
+			Stopped: false,
+		})
+		return
+	}
+
+	//if container doesn't exists returns response
 	response.WriteJson(w, models.StopResponse{
-		Exists:  exists,
-		Stopped: true,
+		Exists:  false,
+		Stopped: false,
 	})
+
 }
