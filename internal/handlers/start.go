20c969d3a66244c04f501cfcc4ccfa13d916618b
jack-san-145 <santhosh34848550@gmail.com>
2026-05-26 16:30:32 +0000

    feat(start): integrate helper to unfreeze container on lxr start

--- internal/handlers/start.go
+++ internal/handlers/start.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"log"
 	"lxr-d/internal/models"
 	"lxr-d/internal/response"
 	"net/http"
@@ -12,7 +11,7 @@ func (h *Handler) StartHandler(w http.ResponseWriter, r *http.Request) {
 	con_name := r.URL.Query().Get("container_name")
 
 	//check whether the container is active or not
-	active := h.Helper.ContainerActive(con_name)
+	active := h.Helper.IsContainerActive(con_name)
 	if active {
 		response.WriteJson(w, models.StartResponse{
 			AlreadyActive: true,
@@ -20,39 +19,27 @@ func (h *Handler) StartHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//check whether the container is exists or not
-	if con, ok := h.Helper.ContainerManager.AllContainers[con_name]; ok {
-
-		//start container with rootfs
-		err := h.Helper.ContainerSetup(con)
+	//check whether the container is exists or not ,if exists make it active
+	exists := h.Helper.ContainerExists(con_name)
+	if exists {
+		err := h.Helper.UnfreezeContainer(con_name)
 		if err != nil {
+
 			response.WriteJson(w, models.StartResponse{
 				Failed: true,
 			})
-			return
-		}
-		h.Helper.ContainerManager.ActiveContainers[con.ContainerName] = &con.PID
-
-		//setup networking for container
-		err = h.Helper.SetupContainerNetworking(con)
-		if err != nil {
-			log.Println("Error during Container Networking: ", err)
+		} else {
 			response.WriteJson(w, models.StartResponse{
-				Failed: true,
+				Activated: true,
 			})
-			return
 		}
 
-		//returns activated response
-		response.WriteJson(w, models.StartResponse{
-			Activated: true,
-		})
 		return
-
 	}
 
 	//if container doesn't exists returns response
 	response.WriteJson(w, models.StartResponse{
 		DoesNotExists: true,
 	})
+
 }
