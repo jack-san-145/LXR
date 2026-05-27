36306c6342d9a3e7806d82844bb5f1fda5ac5439
jack-san-145 <santhosh34848550@gmail.com>
2026-05-27 19:41:20 +0000

    chore(script): use apt-get for non-interactive installation and bind code-server to port 9000

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive  
 
-set -ex 
+echo "[+] Installing dependencies..."
 
 #mount temporary fs
 mount -t tmpfs tmpfs /tmp 
@@ -9,25 +10,34 @@ mount -t tmpfs tmpfs /tmp
 chmod 1777 /tmp 
 
 #update apt 
-apt update 
+apt-get update > /dev/null 2>&1
 
 #install nano editor
-apt install nano
+apt-get install -y nano > /dev/null 2>&1
+echo "  [+] nano ✔"
 
 #install ip package to use all the iptables oerations
-apt install iproute2 -y 
+apt-get install -y iproute2 > /dev/null 2>&1
+echo "  [+] iproute2 ✔"
 
 #install ping globally orelse between containers
-apt install iputils-ping 
+apt-get install -y iputils-ping > /dev/null 2>&1
+echo "  [+] iputils-ping ✔"
+
 
 #install git
-apt install git
+apt-get install -y git > /dev/null 2>&1
+echo "  [+] git ✔"
+
+
+echo "  [+] Installing code-server..."
+echo "  [+] This may take a few minutes. Please wait while the installation completes."
 
 #install code server to use vscode on web
 curl -fsSL https://code-server.dev/install.sh | sh 
 
 #start code-server in background 
-code-server --bind-addr 0.0.0.0:8080 &
+code-server --bind-addr 0.0.0.0:9000 &
 
 #immediately store code-server's background pid to kill
 cs_pid=$!
@@ -38,21 +48,29 @@ sleep 2
 #kill code-server pid 
 kill -2 $cs_pid
 
+
+
 #when code-server starts that created directory with config.yaml 
 #so after that code-server pid killed and now change code-server's password to config file
->  ~/.config/code-server/config.yaml cat << EOF
-bind-addr: 127.0.0.1:8080
+
+echo -e "\n  [+] configuring code-server.."
+
+cat << EOF > ~/.config/code-server/config.yaml
+bind-addr: 127.0.0.1:9000
 auth: password
 password: $PASSWORD
 cert: false
 EOF
 
+
 #run again code-server in background and redirects its output,errors to null
-nohup code-server --bind-addr 0.0.0.0:8080 > /dev/null 2>&1 &
+nohup code-server --bind-addr 0.0.0.0:9000 > /dev/null 2>&1 &
+echo "  [+] code-server ✔"
 
 sleep 2 
 
 #create both randam and urandom for random number generation
-mknod -m 666 /dev/random c 1 8
-mknod -m 666 /dev/urandom c 1 9
+(mknod -m 666 /dev/random c 1 8 || true) > /dev/null 2>&1
+(mknod -m 666 /dev/urandom c 1 9 || true) > /dev/null 2>&1
 
+echo "  [+] Dependency Installation completed ✔"
