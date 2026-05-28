2f1206531e2c4e8bba97af8cdceb713722e7b020
jack-san-145 <santhosh34848550@gmail.com>
2026-05-28 13:36:22 +0000

    feat(kill): unmount overlayfs merged layer when container is destroyed

--- script/container-kill.sh
+++ script/container-kill.sh
@@ -6,10 +6,18 @@ set -ex
 con_dir="/home/LXR/LXR-data/$CONTAINER_NAME"
 
 #remove bridge veth and another veth automatically removed
-ip link delete $BRIDGE_VETH
+ip link delete $BRIDGE_VETH 
+
+#kill container's parent process(unshare)
+kill -9 $CONTAINER_PID
+
+sleep 2
+
+#umount container's overlay setup
+umount -l $con_dir/$IMAGE_NAME/merged
 
 #remove entire container directory
 rm -rf $con_dir
 
-#kill container's parent process(unshare)
-kill -9 $CONTAINER_PID
+
+
