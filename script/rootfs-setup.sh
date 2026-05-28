61ca43f4682598120ebc71f6bec8737ff6f05b6e
jack-san-145 <santhosh34848550@gmail.com>
2026-05-28 13:32:06 +0000

    feat(rootfs): implement overlayfs setup for container isolation

--- script/rootfs-setup.sh
+++ script/rootfs-setup.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-#make the output tracable
-# set -ex
+image_path=/home/LXR/LXR-registry/$IMAGE_NAME
+container_path=/home/LXR/LXR-data/$CONTAINER_NAME/$IMAGE_NAME
 
-#create seperate directory for each container with its containerName
-mkdir -p /home/LXR/LXR-data/$CONTAINER_NAME
+#get user with id=1000
+user=$(id -nu 1000)
 
-echo "  [+] extract image rootfs ✔"
-#copy image rootfs Recursively to container data
-cp -r /home/LXR/LXR-registry/$IMAGE_NAME /home/LXR/LXR-data/$CONTAINER_NAME/
+#create seperate directory for container with overlayfs directories
+mkdir -p $container_path/{upper,work,merged}
 
+
+# Create container rootfs using OverlayFS (base + writable layer)
+# - lowerdir: base image rootfs (read-only)
+# - upperdir: container's writable layer (stores changes)
+# - workdir: internal workspace used by kernel
+# - merged: final combined rootfs used by container
+mount -t overlay overlay \
+  -o lowerdir=$image_path/rootfs,\
+upperdir=$container_path/upper,\
+workdir=$container_path/work \
+  $container_path/merged
+
+echo "  [+] overlay filesystem ✔"
+
+
+mkdir -p $container_path/merged/home/script
+cp /home/jack/LXR/LXR-d/script/install-dependencies.sh $container_path/merged/home/script
 
 echo "  [+] add dependency script ✔"
-mkdir -p /home/LXR/LXR-data/$CONTAINER_NAME/$IMAGE_NAME/rootfs/home/script
-cp /home/jack/LXR/LXR-d/script/install-dependencies.sh /home/LXR/LXR-data/$CONTAINER_NAME/$IMAGE_NAME/rootfs/home/script
 
-#enter into it
-cd /home/LXR/LXR-data/$CONTAINER_NAME/$IMAGE_NAME
 
 #change the rootfs ownership
-chown jack:lxr -R rootfs
+chown $user:lxr $container_path/upper
 
 #give permissions for /dev to create null further
-chmod 775 rootfs/dev
+mkdir -p $container_path/merged/dev
+chmod 775 $container_path/merged/dev
 
 echo "  [+] Rootfs setup completed ✔"
 
