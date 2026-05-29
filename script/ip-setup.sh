5e1e78e4af8f50b4cb11c0cf36a201468438ab6f
jack-san-145 <santhosh34848550@gmail.com>
2026-05-29 19:11:13 +0000

    style: refine formatting of IP setup logs

--- script/ip-setup.sh
+++ script/ip-setup.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-# set -ex
+set -e
 
-echo -e "   [+] creating new veth pairs ✔"
+echo "   [+] creating new veth pairs ✔"
 #creationg new veth pairs 
 ip link add $CONTAINER_VETH type veth peer $BRIDGE_VETH
 
 
-echo -e "   [+] attach to lxr bridge ✔"
 #attach one side veth to bridge lxr0
 ip link set $BRIDGE_VETH master lxr0
+echo "   [+] attach to lxr bridge ✔"
 
 #bring bridge interface up
 ip link set $BRIDGE_VETH up
 
 #move another side veth to container from host
 ip link set $CONTAINER_VETH netns $CONTAINER_PID
+echo "   [+] attach to container ✔"
+
 
 #allocate ip address for container
 nsenter -t $CONTAINER_PID --net \
     ip addr add $CONTAINER_IP dev $CONTAINER_VETH
+echo "   [+] allocate IP address ✔"
+
 
 #bring container interface up
 nsenter -t $CONTAINER_PID --net \
@@ -29,25 +33,13 @@ nsenter -t $CONTAINER_PID --net \
 #bring conainer's loopback interface up
 nsenter -t $CONTAINER_PID --net \
     ip link set lo up
+echo "   [+] bring container interface up ✔"
 
 
-echo -e "   [+] add container default ghateway ✔"
 #add default route as bridge to route unknown traffic
 nsenter -t $CONTAINER_PID --net \
     ip route add default via $BRIDGE_IP
+echo "   [+] add container default ghateway ✔"
 
 
 echo "  [+] Network setup completed ✔"
-
-
-
-
-
-
-
-
-
-
-
-
-
