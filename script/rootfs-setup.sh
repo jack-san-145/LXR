1083b0bdcd098d31bfb3d8a7db1e0c63ed3935df
jack-san-145 <santhosh34848550@gmail.com>
2026-05-29 17:14:42 +0000

    refactor(rootfs): make overlayfs upper directory writable without ownership changes

--- script/rootfs-setup.sh
+++ script/rootfs-setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-image_path=/home/LXR/LXR-registry/$IMAGE_NAME
-container_path=/home/LXR/LXR-data/$CONTAINER_NAME/$IMAGE_NAME
+set -e
 
-#get user with id=1000
-user=$(id -nu 1000)
+image_path="/home/LXR/LXR-registry/$IMAGE_NAME"
+container_path="/home/LXR/LXR-data/$CONTAINER_NAME/$IMAGE_NAME"
 
-#create seperate directory for container with overlayfs directories
-mkdir -p $container_path/{upper,work,merged}
+# create overlay directories
+mkdir -p "$container_path"/{upper,work,merged}
 
 
 # Create container rootfs using OverlayFS (base + writable layer)
@@ -16,27 +15,26 @@ mkdir -p $container_path/{upper,work,merged}
 # - workdir: internal workspace used by kernel
 # - merged: final combined rootfs used by container
 mount -t overlay overlay \
-  -o lowerdir=$image_path/rootfs,\
-upperdir=$container_path/upper,\
-workdir=$container_path/work \
-  $container_path/merged
+  -o lowerdir="$image_path/rootfs",\
+upperdir="$container_path/upper",\
+workdir="$container_path/work" \
+  "$container_path/merged"
 
 echo "  [+] overlay filesystem ✔"
 
-
-mkdir -p $container_path/merged/home/script
-cp /home/jack/LXR/LXR-d/script/install-dependencies.sh $container_path/merged/home/script
+# add dependency script into container
+mkdir -p "$container_path/merged/home/script"
+cp "/home/jack/LXR/LXR-d/script/install-dependencies.sh" \
+   "$container_path/merged/home/script"
 
 echo "  [+] add dependency script ✔"
 
+#create directory /dev
+mkdir -p "$container_path/merged"/dev
 
-#change the rootfs ownership
-chown $user:lxr $container_path/upper
-
-#give permissions for /dev to create null further
-mkdir -p $container_path/merged/dev
-chmod 775 $container_path/merged/dev
+mkdir -p $container_path/upper/old_root
 
 echo "  [+] Rootfs setup completed ✔"
+
 
 
