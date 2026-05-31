8857ca74f5f3778720d94abab02d6ba4906d95c9
jack-san-145 <santhosh34848550@gmail.com>
2026-05-31 12:02:22 +0000

    feat: add JSON tags to NetworkConfig fields

--- internal/ip/stack.go
+++ internal/ip/stack.go
@@ -4,23 +4,23 @@ import ()
 
 // stack to hold the ip addresses
 type Stack struct {
-	IPPool []string
+	IPPool []string `json:"ip_pool"`
 }
 
 // NetworkConfig to hold the ipstack and details of the network
 type NetworkConfig struct {
-	IPStack         Stack
-	Network         string
-	CIDR            int
-	BridgeName      string
-	BridgeIP        string
-	IPStartRange    string
-	IPEndRange      string
-	NetworkAddr     string
-	BroadcastAddr   string
-	TotalUsableHost int
-	HostUsed        int
-	LastUsedIP      string
+	IPStack         Stack  `json:"ip_stack"`
+	Network         string `json:"network"`
+	CIDR            int    `json:"cidr"`
+	BridgeName      string `json:"bridge_name"`
+	BridgeIP        string `json:"bridge_ip"`
+	IPStartRange    string `json:"ip_start_range"`
+	IPEndRange      string `json:"ip_end_range"`
+	NetworkAddr     string `json:"network_addr"`
+	BroadcastAddr   string `json:"broadcast_addr"`
+	TotalUsableHost int    `json:"total_usable_host"`
+	HostUsed        int    `json:"host_used"`
+	LastUsedIP      string `json:"last_used_ip"`
 }
 
 // function to create a new NetworkConfig
