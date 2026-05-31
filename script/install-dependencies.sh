3425eb951de84d71962c20edf3bee8bd2b6cad81
jack-san-145 <santhosh34848550@gmail.com>
2026-05-31 07:52:43 +0000

    build: create temporary  container with root privilages for dependency installation

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -1,76 +1,55 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive  
 
-echo -e "\n[+] Installing dependencies..."
-
-#mount temporary fs
-mount -t tmpfs tmpfs /tmp 
-
-#change mode for /tmp
-chmod 1777 /tmp 
-
-#update apt 
-apt-get update > /dev/null 2>&1
-
-#install nano editor
-apt-get install -y nano > /dev/null 2>&1
-echo "  [+] nano ✔"
-
-#install ip package to use all the iptables oerations
-apt-get install -y iproute2 > /dev/null 2>&1
-echo "  [+] iproute2 ✔"
-
-#install ping globally orelse between containers
-apt-get install -y iputils-ping > /dev/null 2>&1
-echo "  [+] iputils-ping ✔"
+echo -e "\n[+] Setting up $IMAGE_NAME rootfs..."
 
 
-#install git
-apt-get install -y git > /dev/null 2>&1
-echo "  [+] git ✔"
+#first setup temporary container with host root privilages 
+#directory to store the container's data
+IMAGE_PATH=/home/LXR/LXR-registry/$IMAGE_NAME
+
+mkdir -p $IMAGE_PATH/modified_rootfs
+
+cp -a $IMAGE_PATH/rootfs/. $IMAGE_PATH/modified_rootfs
 
 
-echo "  [+] Installing code-server..."
-echo "  [+] This may take a few minutes. Please wait while the installation completes."
+#change rootfs to modified rootfs
+ROOT_FS=$IMAGE_PATH/modified_rootfs
 
-#install code server to use vscode on web
-curl -fsSL https://code-server.dev/install.sh | sh 
+#after this mount wont affects the host 
+mount --make-rprivate /
 
-#start code-server in background 
-code-server --bind-addr 0.0.0.0:9000 &
+#make the same directory as mount point
+mount --bind $ROOT_FS $ROOT_FS
 
-#immediately store code-server's background pid to kill
-cs_pid=$!
+#create directory named old_root to store the old root(host rootfs)
+mkdir -p $ROOT_FS/old_root
 
-#wait for 2 seconds to ensure server run
-sleep 2
+#it changes the new rootfs as (/ root directory) and move host rootfs to old_root
+pivot_root $ROOT_FS $ROOT_FS/old_root
 
-#kill code-server pid 
-kill -2 $cs_pid
+cd /
+
+#mount proc (virtual filesystem) ,sysfs,tmpfs
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+
+#create fresh /dev inside the new rootfs
+mount -t tmpfs tmpfs /dev
+
+#mount the host's /dev/null to new rootfs /dev/null
+touch /dev/null
+mount --bind /old_root/dev/null /dev/null
+
+#mount pts to /dev/pts and add symlink for ptmx
+mkdir -p /dev/pts
+mount -t devpts devpts /dev/pts
+ln -sf /dev/pts/ptmx /dev/ptmx
+
+#clear cache apt modules
+rm -rf /var/lib/apt/lists/*
+
+#clear old rootfs inside container
+umount -l /old_root
 
 
-
-#when code-server starts that created directory with config.yaml 
-#so after that code-server pid killed and now change code-server's password to config file
-
-echo -e "\n  [+] configuring code-server.."
-
-cat << EOF > ~/.config/code-server/config.yaml
-bind-addr: 127.0.0.1:9000
-auth: password
-password: $PASSWORD
-cert: false
-EOF
-
-
-#run again code-server in background and redirects its output,errors to null
-nohup code-server --bind-addr 0.0.0.0:9000 > /dev/null 2>&1 &
-echo "  [+] code-server ✔"
-
-sleep 2 
-
-#create both randam and urandom for random number generation
-(mknod -m 666 /dev/random c 1 8 || true) > /dev/null 2>&1
-(mknod -m 666 /dev/urandom c 1 9 || true) > /dev/null 2>&1
-
-echo "  [+] Dependency Installation completed ✔"
