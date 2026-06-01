b95927a169a31337b5a1d2d1a06afdf6beb2a5c9
jack-san-145 <santhosh34848550@gmail.com>
2026-06-01 04:12:08 +0000

    feat: validate container existence and state before start

--- internal/handlers/start.go
+++ internal/handlers/start.go
@@ -2,20 +2,33 @@ package handlers
 
 import (
 	"bufio"
+	"encoding/json"
 	"log"
 	"lxr-d/internal/models"
-	// "lxr-d/internal/response"
 	"net/http"
+	// "time"
 )
 
 func (h *Handler) StartHandler(w http.ResponseWriter, r *http.Request) {
 
-	// con_name := r.URL.Query().Get("container_name")
+	type conNameStruct struct {
+		ContainerName string `json:"container_name"`
+	}
+
 	var (
 		conBuilder models.ContainerBuilder
 		buf        *bufio.ReadWriter
-		err        error
+		conName    conNameStruct
 	)
+
+	//initialize conBuilder values
+	conBuilder.Quit = make(chan struct{})
+
+	err := json.NewDecoder(r.Body).Decode(&conName)
+	if err != nil {
+		log.Println("Start container Error: ", err)
+		return
+	}
 
 	//hijack the http connnection and stream i/o in real-time over uds
 	hijacker, ok := w.(http.Hijacker)
@@ -35,6 +48,36 @@ func (h *Handler) StartHandler(w http.ResponseWriter, r *http.Request) {
 	buf.WriteString("\r\n")
 	buf.Flush()
 
-	conBuilder.Conn.Write([]byte("Connection Hijacked"))
+	//use seperate go routine to listen the quit signal for worlflow termination
+	go func() {
+		<-conBuilder.Quit
+		conBuilder.Conn.Close()
+
+	}()
+
+	//check whether the container is exists or not ,if exists make it active
+	container, exists := h.Helper.ContainerExists(conName.ContainerName)
+	if !exists {
+		conBuilder.Conn.Write([]byte("Container Doesn't exists\n"))
+		conBuilder.Quit <- struct{}{}
+		return
+	}
+
+	conBuilder.Container = container
+
+	//check whether the container is active or not
+	active := h.Helper.IsContainerActive(conBuilder.Container.ContainerName)
+	if active {
+		conBuilder.Conn.Write([]byte("Container Already Running...\n"))
+		conBuilder.Quit <- struct{}{}
+		return
+	}
+
+	if container.Freezed {
+		h.Helper.UnfreezeContainer(container.ContainerName)
+		conBuilder.Conn.Write([]byte("Container Running...\n"))
+		conBuilder.Quit <- struct{}{}
+		return
+	}
 
 }
